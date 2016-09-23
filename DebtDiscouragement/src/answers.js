fcd59fb643464d6b4c8cc481abc3a5207b841ca0
Dan Rice <dan.rice.92@outlook.com>
2016-09-23 08:24:23 -0600

    Lots of updates to Debt Discouragement Skill

---
+++ DebtDiscouragement/src/answers.js
@@ -0,0 +1,52 @@
+credit card
+credit cards
+credit
+car loan
+car loans
+car lease
+car leases
+lease a car
+leasing a car
+leasing
+a p r
+personal loan
+personal loans
+mortgage
+mortgages
+house
+houses
+land
+condo
+condos
+apartment
+apartments
+apartment complex
+student loan
+student loans
+student
+timeshare
+timeshares
+phone
+phones
+headphones
+speaker
+bluetooth speaker
+cellphone
+cellphones
+smartphone
+smartphones
+no interest
+zero percent interest
+iphone
+android
+samsung galaxy
+payday loan
+payday loans
+life insurance
+whole life insurance
+term life insurance
+
+module.exports = {
+    "credit card": "Credit cards suck.",
+    "payday loan": "Payday loans really suck."
+};
---
+++ DebtDiscouragement/src/index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var AlexaSkill = require('./AlexaSkill'),
+    answers = require('./answers');
+
+var APP_ID = undefined; //OPTIONAL: replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+
+var HowTo = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+HowTo.prototype = Object.create(AlexaSkill.prototype);
+HowTo.prototype.constructor = HowTo;
+
+HowTo.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    var speechText = "Welcome to the How To Helper. You can ask a question like, should I get a lease on a new car? ... Now, what can I help you with?";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "For instructions on what you can say, please say help me.";
+    response.ask(speechText, repromptText);
+};
+
+// DebtIntent, answers instead of recipes, debtName instead of itemName, debtSlot instead of itemSlot
+
+HowTo.prototype.intentHandlers = {
+    "DebtIntent": function (intent, session, response) {
+        var debtSlot = intent.slots.Debt,
+            debtName;
+        if (debtSlot && debtSlot.value){
+            debtName = debtSlot.value.toLowerCase();
+        }
+
+        var cardTitle = "Answer for " + debtName,
+            answer = answers[debtName],
+            speechOutput,
+            repromptOutput;
+        if (answer) {
+            speechOutput = {
+                speech: answer,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.tellWithCard(speechOutput, cardTitle, answer);
+        } else {
+            var speech;
+            if (debtName) {
+                speech = "I'm sorry, I currently do not know about " + debtName + ". What else can I help with?";
+            } else {
+                speech = "I'm sorry, I currently do not know about that type of debt. What else can I help with?";
+            }
+            speechOutput = {
+                speech: speech,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            repromptOutput = {
+                speech: "What else can I help with?",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.ask(speechOutput, repromptOutput);
+        }
+    },
+
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = "You can ask questions such as, what do you think of buying a TV with a credit card, or, you can say exit... Now, what can I help you with?";
+        var repromptText = "You can say things like, what do you think of payday loans, or you can say exit... Now, what can I help you with?";
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        var repromptOutput = {
+            speech: repromptText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
+};
+
+exports.handler = function (event, context) {
+    var howTo = new HowTo();
+    howTo.execute(event, context);
+};
