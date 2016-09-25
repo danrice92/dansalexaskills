f854f8b8d1fe00d447fb44c8eba0c0f29b5388bc
Dan Rice <dan.rice.92@outlook.com>
2016-09-24 22:25:45 -0600

    Updates to Debt Discouragement results

--- DebtDiscouragement/src/answers.js
+++ DebtDiscouragement/src/answers.js
@@ -1,52 +1,41 @@
-credit card
-credit cards
-credit
-car loan
-car loans
-car lease
-car leases
-lease a car
-leasing a car
-leasing
-a p r
-personal loan
-personal loans
-mortgage
-mortgages
-house
-houses
-land
-condo
-condos
-apartment
-apartments
-apartment complex
-student loan
-student loans
-student
-timeshare
-timeshares
-phone
-phones
-headphones
-speaker
-bluetooth speaker
-cellphone
-cellphones
-smartphone
-smartphones
-no interest
-zero percent interest
-iphone
-android
-samsung galaxy
-payday loan
-payday loans
-life insurance
-whole life insurance
-term life insurance
-
 module.exports = {
-    "credit card": "Credit cards are a scam.",
-    "payday loan": "Payday loans are an even bigger scam."
+    "credit card": "Credit cards have some nice cash-back and frequent flyer mile incentives, but they are often more trouble than they're worth. Because they have extremely low minimum payments and extremely high interest rates, you can often end up paying way more on a credit card than you spent in the first place. I would recommend paying for things with a debit card instead to avoid that danger, which is generally not worth the incentives.",
+    "credit cards": "Credit cards have some nice cash-back and frequent flyer mile incentives, but they are often more trouble than they're worth. Because they have extremely low minimum payments and extremely high interest rates, you can often end up paying way more on a credit card than you spent in the first place. I would recommend paying for things with a debit card instead to avoid that danger, which is generally not worth the incentives.",
+    "car loan": "Car loans are a dangerous kind of debt because cars lose their value so quickly, especially when buying a new car. On average, new cars lose sixty-three percent of their value in five years. Saving up to buy a used car with cash or getting a car loan on an inexpensive, used car if you need one now will save you a lot of money, both on the principle loan and the interest.",
+    "car loans": "Car loans are a dangerous kind of debt because cars lose their value so quickly, especially when buying a new car. On average, new cars lose sixty-three percent of their value in five years. Saving up to buy a used car with cash or getting a car loan on an inexpensive, used car if you need one now will save you a lot of money, both on the principle loan and the interest.",
+    "car lease": "Car leases may look attractive at first because the monthly payments are typically lower than on a car loan, but on average they ultimately cost more. Car dealerships typically charge fees if you drive the car too much or have even the slightest scratches and dings on it when the lease ends. I would recommend saving to buy a car with cash or, if you need a car now, getting a used car with a loan instead.",
+    "car leases": "Car leases may look attractive at first because the monthly payments are typically lower than on a car loan, but on average they ultimately cost more. Car dealerships typically charge fees if you drive the car too much or have even the slightest scratches and dings on it when the lease ends. I would recommend saving to buy a car with cash or, if you need a car now, getting a used car with a loan instead.",
+    "lease a car": "Car leases may look attractive at first because the monthly payments are typically lower than on a car loan, but on average they ultimately cost more. Car dealerships typically charge fees if you drive the car too much or have even the slightest scratches and dings on it when the lease ends. I would recommend saving to buy a car with cash or, if you need a car now, getting a used car with a loan instead.",
+    "leasing": "Car leases may look attractive at first because the monthly payments are typically lower than on a car loan, but on average they ultimately cost more. Car dealerships typically charge fees if you drive the car too much or have even the slightest scratches and dings on it when the lease ends. I would recommend saving to buy a car with cash or, if you need a car now, getting a used car with a loan instead.",
+    "a p r": "Car companies often have promotions offering no interest for several months, but remember that this does not mean you will never pay interest on the car and that new cars lose sixty-three percent of their value in five years. Even the best interest rate deals don't compete with purchasing a car interest-free.",
+    "personal loan": "Personal loans are best to avoid if possible since you aren't acquiring any property or knowledge by going into this type of debt. Like all other types of debt, I would recommend paying off personal loans as fast as possible to avoid paying more interest on it than you need to.",
+    "personal loans": "Personal loans are best to avoid if possible since you aren't acquiring any property or knowledge by going into this type of debt. Like all other types of debt, I would recommend paying off personal loans as fast as possible to avoid paying more interest on it than you need to.",
+    "mortgage": "Buying real estate is the only type of debt that can ultimately result in you making more money than you lost because real estate almost always rises in value. If you are looking at buying property or paying it off, however, it is still good to try and avoid paying more interest than necessary, which means paying a high down payment and having a short-term, fixed-rate mortgage.",
+    "mortgages": "Buying real estate is the only type of debt that can ultimately result in you making more money than you lost because real estate almost always rises in value. If you are looking at buying property or paying it off, however, it is still good to try and avoid paying more interest than necessary, which means paying a high down payment and having a short-term, fixed-rate mortgage.",
+    "house": "Buying real estate is the only type of debt that can ultimately result in you making more money than you lost because real estate almost always rises in value. If you are looking at buying property, however, it is still good to try and avoid paying more interest than necessary, which means paying a high down payment and having a short-term, fixed-rate mortgage.",
+    "houses": "Buying real estate is the only type of debt that can ultimately result in you making more money than you lost because real estate almost always rises in value. If you are looking at buying property, however, it is still good to try and avoid paying more interest than necessary, which means paying a high down payment and having a short-term, fixed-rate mortgage.",
+    "land": "Buying real estate is the only type of debt that can ultimately result in you making more money than you lost because real estate almost always rises in value. If you are looking at buying property, however, it is still good to try and avoid paying more interest than necessary, which means paying a high down payment and having a short-term, fixed-rate payment.",
+    "condo": "Buying real estate is the only type of debt that can ultimately result in you making more money than you lost because real estate almost always rises in value. If you are looking at buying property, however, it is still good to try and avoid paying more interest than necessary, which means paying a high down payment and having a short-term, fixed-rate mortgage.",
+    "condos": "Buying real estate is the only type of debt that can ultimately result in you making more money than you lost because real estate almost always rises in value. If you are looking at buying property, however, it is still good to try and avoid paying more interest than necessary, which means paying a high down payment and having a short-term, fixed-rate mortgage.",
+    "apartment": "It can be lucrative to own an apartment or apartment complex and rent it out, and it can also be a good short-term living solution since it does not require a long-term commitment or as high a down payment as buying property. However, buying a home is typically more profitable in the long term if you can manage the down payment and don't plan on moving much in the near future.",
+    "apartments": "It can be lucrative to own an apartment or apartment complex and rent it out, and it can also be a good short-term living solution since it does not require a long-term commitment or as high a down payment as buying property. However, buying a home is typically more profitable in the long term if you can manage the down payment and don't plan on moving much in the near future.",
+    "apartment complex": "It can be lucrative to own an apartment or apartment complex and rent it out, and it can also be a good short-term living solution since it does not require a long-term commitment or as high a down payment as buying property. However, buying a home is typically more profitable in the long term if you can manage the down payment and don't plan on moving much in the near future.",
+    "student loan": "Student loans can be scary to deal with because they cannot be forgiven by bankruptcy and often have high interest rates. Like all types of debt, it is best to pay them off as fast as possible to avoid paying unnecessary interest, but if you are struggling to pay the minimum payments, many student loans offer income-based repayment plans.",
+    "student loans": "Student loans can be scary to deal with because they cannot be forgiven by bankruptcy and often have high interest rates. Like all types of debt, it is best to pay them off as fast as possible to avoid paying unnecessary interest, but if you are struggling to pay the minimum payments, many student loans offer income-based repayment plans.",
+    "student": "Student loans can be scary to deal with because they cannot be forgiven by bankruptcy and often have high interest rates. Like all types of debt, it is best to pay them off as fast as possible to avoid paying unnecessary interest, but if you are struggling to pay the minimum payments, many student loans offer income-based repayment plans.",
+    "timeshare": "Timeshares often sound good when the salesman tells you about them, but the average timeshare costs fourteen thousand dollars a year and only allows you to use your vacation home one or two weeks a year. Plus, they are extremely difficult to get out of. I would recommend avoiding them at all costs.",
+    "timeshares": "Timeshares often sound good when the salesman tells you about them, but the average timeshare costs fourteen thousand dollars a year and only allows you to use your vacation home one or two weeks a year. Plus, they are extremely difficult to get out of. I would recommend avoiding them at all costs.",
+    "phone": "Smartphones are typically financed with no interest, so they are not particularly dangerous things to put on payments. However, you should remember that phone carrier deals offering low down payments will still require you to ultimately pay the phone off. Like most purchases, a helpful way to avoid overspending is to only buy things you can afford to purchase at full price, in cash, upfront.",
+    "phones": "Smartphones are typically financed with no interest, so they are not particularly dangerous things to put on payments. However, you should remember that phone carrier deals offering low down payments will still require you to ultimately pay the phone off. Like most purchases, a helpful way to avoid overspending is to only buy things you can afford to purchase at full price, in cash, upfront.",
+    "cell phone": "Smartphones are typically financed with no interest, so they are not particularly dangerous things to put on payments. However, you should remember that phone carrier deals offering low down payments will still require you to ultimately pay the phone off. Like most purchases, a helpful way to avoid overspending is to only buy things you can afford to purchase at full price, in cash, upfront.",
+    "cell phones": "Smartphones are typically financed with no interest, so they are not particularly dangerous things to put on payments. However, you should remember that phone carrier deals offering low down payments will still require you to ultimately pay the phone off. Like most purchases, a helpful way to avoid overspending is to only buy things you can afford to purchase at full price, in cash, upfront.",
+    "smart phone": "Smartphones are typically financed with no interest, so they are not particularly dangerous things to put on payments. However, you should remember that phone carrier deals offering low down payments will still require you to ultimately pay the phone off. Like most purchases, a helpful way to avoid overspending is to only buy things you can afford to purchase at full price, in cash, upfront.",
+    "smart phones": "Smartphones are typically financed with no interest, so they are not particularly dangerous things to put on payments. However, you should remember that phone carrier deals offering low down payments will still require you to ultimately pay the phone off. Like most purchases, a helpful way to avoid overspending is to only buy things you can afford to purchase at full price, in cash, upfront.",
+    "no interest": "While it is true that debt with no interest is not as dangerous or costly as debt with interest, it is good to remember to read the fine print before you sign contracts or financing agreements. Things that sound too good to be true often are, and even with no interest, a debt still means that you owe someone money and will have to pay them back eventually.",
+    "zero percent interest": "While it is true that debt with no interest is not as dangerous or costly as debt with interest, it is good to remember to read the fine print before you sign contracts or financing agreements. Things that sound too good to be true often are, and even with no interest, a debt still means that you owe someone money and will have to pay them back eventually.",
+    "payday loan": "Payday loans are essentially a scam. They often have absurdly high interest rates and cost you far more than the money you make. Avoid them like the plague and get out of them as fast as possible.",
+    "payday loans": "Payday loans are essentially a scam. They often have absurdly high interest rates and cost you far more than the money you make. Avoid them like the plague and get out of them as fast as possible.",
+    "cash advance": "Payday loans are essentially a scam. They often have absurdly high interest rates and cost you far more than the money you make. Avoid them like the plague and get out of them as fast as possible.",
+    "pay in advance": "Payday loans are essentially a scam. They often have absurdly high interest rates and cost you far more than the money you make. Avoid them like the plague and get out of them as fast as possible.",
+    "life insurance": "Term life insurance is typically a better choice than whole life insurance. Term life insurance is a good value because you are not locked into paying it for life in order to get anything out of it, it usually costs less, and it is a more straightforward program. Whole life insurance requires you to pay into it indefinitely, it usually costs more to get the same coverage, and the investment opportunities they sometimes offer are not as profitable as most retirement plans are. In almost all cases, term insurance is a better option."
 };
